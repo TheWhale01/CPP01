94b095af2b8dd7dfe181bdb6b7f82ac9d0942dbe
Hugo BRETECHE <hubretec@e1r3p19.clusters.42paris.fr>
2022-09-05 15:52:27 +0200

    ex05 done

---
+++ ex05/Harl.cpp
@@ -0,0 +1,41 @@
+#include "Harl.hpp"
+
+Harl::Harl( void )
+{
+	return ;
+}
+
+Harl::~Harl( void )
+{
+	return ;
+}
+
+void	Harl::complain(std::string level) const
+{
+	void (Harl::*funcptr[4])(void) const = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++)
+		if (levels[i] == level)
+			(this->*funcptr[i])();
+	return ;
+}
+
+void	Harl::_debug(void) const
+{
+	std::cout << "[DEBUG]" << std::endl << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl << std::endl;
+}
+
+void	Harl::_info(void) const
+{
+	std::cout << "[INFO]" << std::endl << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
+}
+
+void	Harl::_warning(void) const
+{
+	std::cout << "[WARNING]" << std::endl << " think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl << std::endl;
+}
+
+void	Harl::_error(void) const
+{
+	std::cout << "[ERROR]" << std::endl << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
+}
