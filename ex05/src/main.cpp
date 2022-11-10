ad75b7323491997c13e97cdb6350ac70843531a0
TheWhale01 <ard.rasp01@gmail.com>
2022-11-10 12:32:26 +0100

    added tests for ex05

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -4,6 +4,14 @@ int main(void)
 {
 	Harl harl;
 
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl;
 	harl.complain("ERROR");
+	std::cout << std::endl;
+	harl.complain("nothing");
 	return (0);
 }
